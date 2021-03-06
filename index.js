9157a2cb674faf3a0b43f54760eedcd332344e52
Kira McLean <email@kiramclean.com>
2021-03-06 12:10:20 -0500

    notes

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
 console.log("Hello world!")
+
+// https://dev.to/iulianoctavianpreda/run-your-npx-script-directly-from-github-create-your-own-cli-commands-and-other-stories-4pn3
+// https://github.com/filipesilva/create-cljs-app/tree/master/src
+// https://github.com/minimal-xyz/minimal-shadow-cljs-browser/blob/master/assets/index.html
